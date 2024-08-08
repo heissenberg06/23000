c7553217cc872358c1920f039901e38755e3c83b
Berko Pasha <berk579.32@gmail.com>
2024-08-08 13:12:05 +0300

    authenticate pass

--- client/src/pages/registerpage.js
+++ client/src/pages/registerpage.js
@@ -8,19 +8,24 @@ const RegisterPage = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState(''); // New state for confirmation password
   const [message, setMessage] = useState(''); // State for displaying messages
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match'); // Error message if passwords do not match
+      return;
+    }
     try {
       const response = await axios.post('http://localhost:3001/api/auth/register', {
         username,
         email,
         password,
       });
-      setMessage(response.data.message + " You are directed to the login. "); // Display success message
-      setTimeout(() => navigate('/login'), 2000)
+      setMessage(response.data.message + " You are directed to the login."); // Display success message
+      setTimeout(() => navigate('/login'), 2000);
     } catch (error) {
       setMessage(error.response.data.message || 'Registration failed'); // Display error message
     }
@@ -58,6 +63,16 @@ const RegisterPage = () => {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               required
             />
           </div>
